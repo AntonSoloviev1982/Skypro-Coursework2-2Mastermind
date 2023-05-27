a17a4a2e42a1c4c7565a3736110f76c67204b8bb
Anton <Soloviev.gvardiya@gmail.com>
2023-05-27 20:23:10 +0300

    Initial commit

---
+++ src/main/java/com/example/skyprocoursework22mastermind/SkyproCoursework22MastermindApplication.java
@@ -0,0 +1,13 @@
+package com.example.skyprocoursework22mastermind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproCoursework22MastermindApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyproCoursework22MastermindApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/skyprocoursework22mastermind/SkyproCoursework22MastermindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skyprocoursework22mastermind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproCoursework22MastermindApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
